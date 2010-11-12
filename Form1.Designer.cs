8b129da270d08a2008029100ed53ab2c840a2415
Snoop <snoopinf@gmail.com>
2010-11-12 20:52:49 +0200

    +deletebutton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,14 +54,16 @@
             this.cursor = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.delete = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
-            this.toolStrip1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripSeparator12,
@@ -80,10 +82,11 @@
             this.toolStripSeparator8,
             this.open,
             this.toolStripSeparator9,
-            this.toolStripSeparator10});
+            this.toolStripSeparator10,
+            this.delete});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(730, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -162,8 +165,7 @@
             this.save.Image = ((System.Drawing.Image)(resources.GetObject("save.Image")));
             this.save.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.save.Name = "save";
-            this.save.Size = new System.Drawing.Size(51, 22);
-            this.save.Text = "Save";
+            this.save.Size = new System.Drawing.Size(23, 22);
             this.save.ToolTipText = "Сохранить проект";
             this.save.Click += new System.EventHandler(this.save_Click);
             // 
@@ -182,8 +184,7 @@
             this.open.Image = ((System.Drawing.Image)(resources.GetObject("open.Image")));
             this.open.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.open.Name = "open";
-            this.open.Size = new System.Drawing.Size(56, 22);
-            this.open.Text = "Open";
+            this.open.Size = new System.Drawing.Size(23, 22);
             this.open.ToolTipText = "Открыть проект";
             this.open.Click += new System.EventHandler(this.open_Click);
             // 
@@ -200,71 +201,74 @@
             // P
             // 
             this.P.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.P.Location = new System.Drawing.Point(0, 83);
+            this.P.Image = ((System.Drawing.Image)(resources.GetObject("P.Image")));
+            this.P.Location = new System.Drawing.Point(1, 56);
             this.P.Name = "P";
-            this.P.Size = new System.Drawing.Size(51, 46);
+            this.P.Size = new System.Drawing.Size(55, 55);
             this.P.TabIndex = 1;
             this.P.Tag = "1";
-            this.P.Text = "P";
             this.P.UseVisualStyleBackColor = true;
             // 
             // t
             // 
             this.t.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.t.Location = new System.Drawing.Point(0, 126);
+            this.t.Image = ((System.Drawing.Image)(resources.GetObject("t.Image")));
+            this.t.Location = new System.Drawing.Point(1, 109);
             this.t.Name = "t";
-            this.t.Size = new System.Drawing.Size(51, 46);
+            this.t.Size = new System.Drawing.Size(55, 55);
             this.t.TabIndex = 2;
             this.t.Tag = "2";
             this.t.Text = "t";
             this.t.UseVisualStyleBackColor = true;
+            this.t.Click += new System.EventHandler(this.t_Click);
             // 
             // tt
             // 
             this.tt.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.tt.Location = new System.Drawing.Point(0, 169);
+            this.tt.Image = ((System.Drawing.Image)(resources.GetObject("tt.Image")));
+            this.tt.Location = new System.Drawing.Point(1, 161);
             this.tt.Name = "tt";
-            this.tt.Size = new System.Drawing.Size(51, 46);
+            this.tt.Size = new System.Drawing.Size(55, 55);
             this.tt.TabIndex = 3;
             this.tt.Tag = "3";
-            this.tt.Text = "tt";
             this.tt.UseVisualStyleBackColor = true;
             // 
             // connector
             // 
             this.connector.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.connector.Location = new System.Drawing.Point(0, 212);
+            this.connector.Image = ((System.Drawing.Image)(resources.GetObject("connector.Image")));
+            this.connector.Location = new System.Drawing.Point(1, 214);
             this.connector.Name = "connector";
-            this.connector.Size = new System.Drawing.Size(51, 46);
+            this.connector.Size = new System.Drawing.Size(55, 55);
             this.connector.TabIndex = 4;
             this.connector.Tag = "4";
-            this.connector.Text = "line";
             this.connector.UseVisualStyleBackColor = true;
             // 
             // cursor
             // 
             this.cursor.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cursor.Location = new System.Drawing.Point(0, 41);
+            this.cursor.Image = ((System.Drawing.Image)(resources.GetObject("cursor.Image")));
+            this.cursor.Location = new System.Drawing.Point(1, 3);
             this.cursor.Name = "cursor";
-            this.cursor.Size = new System.Drawing.Size(51, 46);
+            this.cursor.Size = new System.Drawing.Size(55, 55);
             this.cursor.TabIndex = 0;
             this.cursor.Tag = "0";
-            this.cursor.Text = "cursor";
             this.cursor.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(0, 264);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(1, 278);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(51, 38);
+            this.button2.Size = new System.Drawing.Size(55, 55);
             this.button2.TabIndex = 5;
-            this.button2.Text = "clean";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.clean_Click);
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.IsSplitterFixed = true;
             this.splitContainer1.Location = new System.Drawing.Point(0, 25);
             this.splitContainer1.Name = "splitContainer1";
@@ -279,28 +283,42 @@
             this.splitContainer1.Panel1.Controls.Add(this.t);
             this.splitContainer1.Panel1.Controls.Add(this.P);
             this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            this.splitContainer1.Panel1MinSize = 55;
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.Window;
             this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-            this.splitContainer1.Size = new System.Drawing.Size(730, 316);
-            this.splitContainer1.SplitterDistance = 52;
+            this.splitContainer1.Size = new System.Drawing.Size(784, 337);
+            this.splitContainer1.SplitterDistance = 57;
             this.splitContainer1.SplitterWidth = 1;
             this.splitContainer1.TabIndex = 0;
+            // 
+            // delete
+            // 
+            this.delete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.delete.Image = ((System.Drawing.Image)(resources.GetObject("delete.Image")));
+            this.delete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(23, 22);
+            this.delete.Text = "DeletButton";
+            this.delete.ToolTipText = "Удалить объект";
+            this.delete.Click += new System.EventHandler(this.delete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(730, 341);
+            this.ClientSize = new System.Drawing.Size(784, 362);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -334,6 +352,7 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator10;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator12;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator11;
+        private System.Windows.Forms.ToolStripButton delete;
 
     }
 }
