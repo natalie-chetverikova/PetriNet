f058284821d135dae81b5c4d221ed197e120977e
Snoop <snoopinf@gmail.com>
2010-11-12 20:55:27 +0200

    changes

--- Position.cs
+++ Position.cs
@@ -9,17 +9,26 @@ namespace PetriNets
     [Serializable]
     public class Position
     {
+        public static int Id_cntr
+        { get; set; }
+
         public int ID
         { get; set; }
         public int Tokens
         { get; set; }
         public Dictionary<Transition, int> DictOfIn
         { get; set; }
-        
-        
-        public Position(int ID)
+
+        public System.Drawing.Point Location
+        { get; set; }
+
+        public int Fieldnumber
+        { get; set; }
+
+        public Position(int fieldnum)
         {
-            this.ID = ID;
+            this.Fieldnumber = fieldnum;   
+            this.ID = Id_cntr++;
             Tokens = 0;
             DictOfIn = new Dictionary<Transition, int>();            
         }
