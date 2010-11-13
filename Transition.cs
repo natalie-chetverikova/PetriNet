118d6fb86bb866513315bf85ddfb4d6ea0d08a9c
Snoop <snoopinf@gmail.com>
2010-11-13 16:14:34 +0200

    k

---
+++ Transition.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace PetriNets
+{
+    [Serializable]
+    public class Transition
+    {
+        //порядковый номер в листе переходов
+        public static int Id_cntr
+        { get; set; }
+        public int ID
+        { get; set; }
+        public int Fieldnumber
+        { get; set; }
+        public string Type
+        { get; set; }
+        public double Time
+        { get; set; }
+        public Dictionary<Position, int> DictOfIn
+        { get; set; }
+
+        public System.Drawing.Point Location
+        { get; set; }
+
+        public Transition(int fieldnumber)
+        {
+            ID = Id_cntr++;
+            this.Fieldnumber = fieldnumber;
+            DictOfIn = new Dictionary<Position, int>();
+            //мгновенный
+            if (ID % 2 == 1)
+            {
+                Type = "momentary";
+                Time = 0;
+            }
+                //временной
+            else
+            {
+                Type = "time";
+                Time = 1; //потом поменять
+            }
+        }
+
+        public void addIn(Position o)
+        {
+            if (DictOfIn.ContainsKey(o))
+            {
+                DictOfIn[o]++;
+            }
+            else
+            {
+                DictOfIn.Add(o, 1);
+            }
+        }
+
+        public void deleteIn(Position o)
+        {
+            DictOfIn[o]--;            
+        }
+    }
+}
